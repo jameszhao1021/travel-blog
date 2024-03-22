fadf31757a53ef76d3be4e810d9456cb4ce2e81e
hayley <hayleyhkim@gmail.com>
2024-03-22 13:42:50 +1100

    skeleton of my blog page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# MERN-Stack Infrastructure
+# Travel Blog by James, MahShid, Jordan and Hayley
 
 Clone this repo to provide the starter code for a comprehensive MERN-Stack project including token-based authentication.
--- src/pages/MyBlogPage/MyBlogPage.jsx
+++ src/pages/MyBlogPage/MyBlogPage.jsx
@@ -1,11 +1,50 @@
 import React from "react";
+import './myBlogPage.css';
 
 function MyBlogPage(){
 
 
     return(
         <>
-        <h1>MyBlogPage</h1>
+        
+        <div className="row mt-3 mb-3">
+            <div className="col-sm profile">
+                <div>Username</div>
+                <div><img src="../../../public/images/profile.png" className="profileImage"/></div>
+                <div>Bio</div>
+            </div>
+
+            <div className="col-sm">
+                <div>Post count: count </div>
+                <div>More features coming..</div>
+            </div>
+
+            <div className="col-sm">
+                <div><img src="../../../public/images/map.png" className="mapImage"/></div>
+            </div>
+        </div>
+
+        <div className="row mt-3 mb-3">
+            <div className="col-sm">
+                <button>Edit profile</button>
+            </div>
+            <div className="col-sm">
+                <button>New Post</button>
+            </div>
+        </div>
+
+        <div className="row mb-3 col-3">
+            <select>
+                <option value="All Posts">All Posts</option>
+                <option value="Public Posts">Public Posts</option>
+                <option value="Private Posts">Private Posts</option>
+            </select> 
+        </div>
+
+        <div className="row">
+            <div >Article Card goes here</div>
+        </div>
+        
         </>
     )
 }
