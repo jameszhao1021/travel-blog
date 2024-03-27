1a4acbdd6c12b08aed8d55a1156815390aa1a82f
mahshidmn <ma.mojtabavi@gmail.com>
2024-03-27 16:56:46 +1100

    test

--- test.jsx
+++ test.jsx
@@ -1,86 +1,38 @@
-const Blog = require('../../models/blog');
+import React, { useState } from 'react';
+import Navbar from './Navbar'; // Assume you've created this component
 
+const blogItems = [
+  { id: 1, name: 'Apple', category: 'Fruit' },
+  { id: 2, name: 'Carrot', category: 'Vegetable' },
+  { id: 3, name: 'Banana', category: 'Fruit' },
+  // Add more items as needed
+];
 
+function App() {
+  const [searchResults, setSearchResults] = useState(blogItems);
 
+  const handleSearch = (searchTerm) => {
+    const filteredItems = sampleItems.filter(item =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      item.category.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setSearchResults(filteredItems);
+  };
 
-  
-  async function addComment(req, res)  {
-    // console.log(req.body);
-    // console.log(req.params);
-    try {
-      const blog = await Blog.findById(req.params.id);
-      
-      req.body.user = req.user._id;
-      req.body.userName = req.user.name;
-    //   console.log(req.body.userName);
-    
-      if (!blog) {
-        return res.status(404).json({ message: 'Blog not found' });
-      }
-  
-     const newComment = req.body;
-
-      blog.comments.push(newComment);
-      
-      await blog.save();
-    //   console.log(blog);
-     
-      res.status(201).json(newComment);
-    } catch (error) {
-      console.error('Error creating comment:', error);
-      res.status(500).json({ message: 'Server error' });
-    }
+  return (
+    <div>
+      <Navbar onSearch={handleSearch} />
+      <div>
+        <h2>Search Results</h2>
+        <ul>
+          {searchResults.map(item => (
+            <li key={item.id}>{item.name} - {item.category}</li>
+          ))}
+        </ul>
+      </div>
+      {/* Other components */}
+    </div>
+  );
 }
 
-async function deleteComment(req, res) {
-    try {
-       
-        const blog = await Blog.findOne({ 'comments._id': req.params.id, 'comments.user': req.user._id });
-        console.log(blog);  
-        if (!blog) {
-            return res.status(404).json({error: 'Comment not found'});
-        }
-       blog.comments.remove(req.params.id);
-        await blog.save();
-        res.json({message: 'Comment successfully deleted', id: req.params.id});
-    } catch(err) {
-        console.log(err);
-        res.status(400).json({error: err.message});
-    }
-}
-
-async function updateComment(req,res) {
-    const userId = req.user._id; 
-    const commentId = req.params.id;
-    try {
-       // Find the blog post containing the comment
-        const blog = await Blog.findOne({ 'comments._id': commentId, 'comments.user': userId });
-        if (!blog) {
-          return res.status(404).send({ message: 'Blog post not found' });
-        }
-         // Find the comment within the blog post
-        const comment = blog.comments.id(commentId);
-        if (!comment) {
-            return res.status(404).send({ message: 'Comment not found' });
-          }
-        // Check if the current user is authorized to update the comment
-        if (comment.user.toString() !== userId.toString()) {
-            return res.status(403).send({ message: 'User not authorized to edit this comment' });
-          }
-        // Update the comment text with the new value
-        comment.text = req.body.text; // Assuming the updated text is sent in req.body.text
-        await blog.save();
- } catch (error) {
-    console.error('Error updating comment:', error);
-    res.status(500).send({ message: 'Failed to update comment' });
-  }
-}
-
-
-  module.exports = {
-    addComment,
-    delete: deleteComment,
-    update: updateComment
-  }
-  
-  
+export default App;
