9f2e7a4137982e88ad6260c04a494ede876e2cb7
james <351917842@qq.com>
2024-03-28 09:18:48 +0800

    loading feature

--- src/pages/MyBlogPage/MyBlogPage.jsx
+++ src/pages/MyBlogPage/MyBlogPage.jsx
@@ -86,13 +86,16 @@ function MyBlogPage({ user, uploadImage }) {
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-lg-4 profile">
-                        <div ><img src={profile.picture} className="profileImage" alt="Profile" style={{ maxWidth: '250px', maxHeight: '250px' }} /></div>
+                        {profile.picture ? (
+                            <img src={profile.picture} className="profileImage" alt="Profile" style={{ maxWidth: '250px', maxHeight: '250px' }} />
+                        ) : (
+                            <div className="loading-spinner">Loading...</div>
+                        )}
                         <div>{user.name}</div>
                         <div className="bio">{profile.bio}</div>
                         <button className="btn button-custom" onClick={toggleProfileModal}>Edit profile</button>
                         <ProfileFormModal profile={profile} setProfile={setProfile} uploadImage={uploadImage} showProfileModal={showProfileModal} toggleProfileModal={toggleProfileModal} editProfile={editProfile} setEditProfile={setEditProfile} user={user} newProfile={newProfile} setNewProfile={setNewProfile} />
                     </div>
-
                     <div className="col-lg-8">
                         <InteractiveMap markedCountries={markedCountries} />
                     </div>
