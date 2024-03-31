47889227d6cbefad8bf0fa00de6e90f7e7c4a2ba
mahshidmn <ma.mojtabavi@gmail.com>
2024-03-31 18:26:45 +1100

    search feature added

--- src/components/SearchResultCard.jsx
+++ src/components/SearchResultCard.jsx
@@ -9,7 +9,7 @@ function SearchResultCard({ result }) {
             <div className="col-4 my-2 py-3">
                 <div className='blogCard'>
                     <div className='blogCardBg'>
-                        <Link to={`/search/${result._id}`}>
+                        <Link to={`/blog/${result._id}`}>
                             <div className="d-flex flex-column justify-content-between" style={{ height: '100%' }}>
                             <div className=" col-lg-12" style={{ backgroundImage: `url(${result.preview})`, backgroundSize: 'cover', height: '60%' }}></div>
                                 <div className='blogTitle'>{result.title}</div>
