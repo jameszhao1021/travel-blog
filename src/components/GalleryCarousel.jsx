c43d29441a71d76d527498290bc234338ada4fad
james <351917842@qq.com>
2024-04-03 02:27:04 +0800

    image updated

--- src/components/GalleryCarousel.jsx
+++ src/components/GalleryCarousel.jsx
@@ -1,11 +1,14 @@
 import Carousel from 'react-bootstrap/Carousel';
+import France from '../../public/images/gallery1.png';
+import Greece from '../../public/images/gallery2.png';
+import Australia from '../../public/images/gallery3.png';
 import '../components/GalleryCard.css';
 
 export default function GalleryCarousel() {
     return (
         <Carousel className='my-3'>
           <Carousel.Item>
-            <img src='../../public/images/gallery1.png' className="d-block w-100 carousel-image" alt="slide image"/>
+            <img src={France} className="d-block w-100 carousel-image" alt="slide image"/>
             <Carousel.Caption className='carousel-caption'>
               <div className='carousel-title'>Paris, France</div>
               <p>The "City of Light" and its Eiffel Tower</p>
@@ -13,7 +16,7 @@ export default function GalleryCarousel() {
           </Carousel.Item>
 
           <Carousel.Item>
-          <img src='../../public/images/gallery2.png' className="d-block w-100 carousel-image" alt="slide image"/>
+          <img src={Greece} className="d-block w-100 carousel-image" alt="slide image"/>
             <Carousel.Caption className='carousel-caption'>
               <div className='carousel-title'>Mykonos, Greece</div>
               <p>A slice of paradise</p>
@@ -21,7 +24,7 @@ export default function GalleryCarousel() {
           </Carousel.Item>
 
           <Carousel.Item>
-          <img src='../../public/images/gallery3.png' className="d-block w-100 carousel-image" alt="slideimage"/>
+          <img src={Australia} className="d-block w-100 carousel-image" alt="slideimage"/>
             <Carousel.Caption className='carousel-caption'>
               <div className='carousel-title'>Sydney, Australia</div>
               <p>
